4b000acc0a4fa81f27a5a7a591c19060198e0e22
Matheus Kuss <129921288+Dev-Kuss@users.noreply.github.com>
2024-09-19 16:52:34 -0300

    Add files via upload

---
+++ src/main/java/com/example/manutencaoequipamentos/ManutencaoequipamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.manutencaoequipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoequipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoequipamentosApplication.class, args);
+	}
+
+}
