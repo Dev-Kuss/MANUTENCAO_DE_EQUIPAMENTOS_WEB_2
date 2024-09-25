b5adc2fb1337aefe9b11ad730312244ab83ef108
Dev-Kuss <mkuss.dev@gmail.com>
2024-09-25 17:22:37 -0300

    Alterando classe de teste e de cliente para testar execução

--- src/main/java/com/tads/me/controller/ClienteController.java
+++ src/main/java/com/tads/me/controller/ClienteController.java
@@ -14,72 +14,72 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/cliente")
-class ClienteController {
+// @RestController
+// @RequestMapping("/cliente")
+// class ClienteController {
 
-    @Autowired
-    ClienteRepo repository;
+//     @Autowired
+//     ClienteRepo repository;
 
-    @GetMapping
-    public ResponseEntity<List<Cliente>> getAll() {
-        try {
-            List<Cliente> items = new ArrayList<Cliente>();
+//     @GetMapping
+//     public ResponseEntity<List<Cliente>> getAll() {
+//         try {
+//             List<Cliente> items = new ArrayList<Cliente>();
 
-            repository.findAll().forEach(items::add);
+//             repository.findAll().forEach(items::add);
 
-            if (items.isEmpty())
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//             if (items.isEmpty())
+//                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
-            return new ResponseEntity<>(items, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+//             return new ResponseEntity<>(items, HttpStatus.OK);
+//         } catch (Exception e) {
+//             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//         }
+//     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<Cliente> getById(@PathVariable("id") Long id) {
-        Optional<Cliente> existingItemOptional = repository.findById(id);
+//     @GetMapping("{id}")
+//     public ResponseEntity<Cliente> getById(@PathVariable("id") Long id) {
+//         Optional<Cliente> existingItemOptional = repository.findById(id);
 
-        if (existingItemOptional.isPresent()) {
-            return new ResponseEntity<>(existingItemOptional.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+//         if (existingItemOptional.isPresent()) {
+//             return new ResponseEntity<>(existingItemOptional.get(), HttpStatus.OK);
+//         } else {
+//             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//         }
+//     }
 
-    @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody Cliente item) {
-        try {
-            Cliente savedItem = repository.save(item);
-            return new ResponseEntity<>(savedItem, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
-        }
-    }
+//     @PostMapping
+//     public ResponseEntity<Cliente> create(@RequestBody Cliente item) {
+//         try {
+//             Cliente savedItem = repository.save(item);
+//             return new ResponseEntity<>(savedItem, HttpStatus.CREATED);
+//         } catch (Exception e) {
+//             return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+//         }
+//     }
 
-    @PutMapping("{id}")
-    public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody Cliente item) {
-        Optional<Cliente> existingItemOptional = repository.findById(id);
-        if (existingItemOptional.isPresent()) {
-            Cliente existingItem = existingItemOptional.get();
-            System.out.println("TODO for developer - update logic is unique to entity and must be implemented manually.");
-            //existingItem.setSomeField(item.getSomeField());
-            return new ResponseEntity<>(repository.save(existingItem), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+//     @PutMapping("{id}")
+//     public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody Cliente item) {
+//         Optional<Cliente> existingItemOptional = repository.findById(id);
+//         if (existingItemOptional.isPresent()) {
+//             Cliente existingItem = existingItemOptional.get();
+//             System.out.println("TODO for developer - update logic is unique to entity and must be implemented manually.");
+//             existingItem.setSomeField(item.getSomeField());
+//             return new ResponseEntity<>(repository.save(existingItem), HttpStatus.OK);
+//         } else {
+//             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//         }
+//     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
-        try {
-            repository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
-        }
-    }
-}
+//     @DeleteMapping("{id}")
+//     public ResponseEntity<HttpStatus> delete(@PathVariable("id") Long id) {
+//         try {
+//             repository.deleteById(id);
+//             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//         } catch (Exception e) {
+//             return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+//         }
+//     }
+// }
--- src/main/java/com/tads/me/controller/TestController.java
+++ src/main/java/com/tads/me/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.tads.me.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/test")
-public class testController {
+public class TestController {
 
 
     @GetMapping
