79d86d342a460f329f97aa37f7a902cb536d4aa7
Lucas <lucasmachado1@ufpr.br>
2024-09-26 15:46:14 -0300

    chore: angular material dependency

--- manutencao-equipamentos-front/src/main.ts
+++ manutencao-equipamentos-front/src/main.ts
@@ -3,10 +3,11 @@ import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(), provideAnimationsAsync()
   ]
 }).catch(err => console.error(err));
