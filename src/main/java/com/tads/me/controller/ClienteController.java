ebf1626a672eea04308d00d7f3c52dff11a9becf
Dev-Kuss <mkuss.dev@gmail.com>
2024-10-08 11:37:01 -0300

    Corrigindo migration e criando acesso ao banco do container pela porta 5433

--- src/main/java/com/tads/me/controller/ClienteController.java
+++ src/main/java/com/tads/me/controller/ClienteController.java
@@ -2,13 +2,15 @@ package com.tads.me.controller;
 
 import com.tads.me.domain.cliente.Cliente;
 import com.tads.me.domain.cliente.ClienteRequestDTO;
+import com.tads.me.repositories.ClienteRepository;
 import com.tads.me.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -16,13 +18,24 @@ class ClienteController {
 
     @Autowired
     ClienteService clienteService;
-//
-//    @PostMapping
-//    public void create(@RequestBody ClienteRequestDTO data) {
-//        Cliente clienteData = new Cliente(data);
-//        repository.sa
-////    }ve(clienteData);
-//
+    @Autowired
+    ClienteRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll() {
+        try {
+            List<Cliente> items = new ArrayList<Cliente>();
+
+            repository.findAll().forEach(items::add);
+
+            if (items.isEmpty())
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+            return new ResponseEntity<>(items, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PostMapping
     public ResponseEntity<Cliente> create(@RequestBody ClienteRequestDTO data) {
