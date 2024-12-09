1c2a708dff4466bb14426d1d7e1257a75d03eb19
Dev-Kuss <mkuss.dev@gmail.com>
2024-12-09 15:12:36 -0300

    feat: add sorting functionality and success animation to login component
    - Implemented a new method in SolicitacaoRepository to retrieve solicitacoes ordered by ID in descending order.
    - Updated SolicitacaoService to utilize the new sorting method in the listarSolicitacoes function.
    - Added CSS styles for a success animation in the login component.
    - Enhanced the login component's HTML to include a success animation popup upon successful login.
    - Introduced a new method in LoginComponent to navigate to the signup page.

--- manutencao-equipamentos-back/src/main/java/com/tads/me/repository/SolicitacaoRepository.java
+++ manutencao-equipamentos-back/src/main/java/com/tads/me/repository/SolicitacaoRepository.java
@@ -2,10 +2,15 @@ package com.tads.me.repository;
 
 import com.tads.me.entity.Solicitacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
     List<Solicitacao> findByClienteId(UUID cliente_id);
+
+    default List<Solicitacao> findAllByOrderByIdDesc() {
+        return findAll(Sort.by(Sort.Direction.DESC, "idSolicitacao"));
+    }
 }
